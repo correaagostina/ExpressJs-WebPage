3d7c9c01a3d12dc229e32acdcf5d5b7efc137c72
Correa Agostina Bernardette <agostina.correaok@gmail.com>
2023-08-04 07:20:59 -0300

    agrego views

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const PORT = 3008; 
+
+app.set('view engine','ejs');
+app.use(express.static('views'));
+
+const computerProducts = [{name: 'Notebook Lenovo', price: 720}, {name: 'Table Samsung 64gb', price: 230}, {name: 'MacBook Air 2020', price: 800}]
 
 //Definir ruta basica
 app.get('/',(req, res) =>{
     //res.writeHead(200,{'Contect-type':'text/html'});
-    res.send('<h1>Bienvenidas a nuestra web!</h1>');
+    const data = {
+        title: "Sitio web",
+        message: "Esto es una prueba para ver el message.",
+        products: computerProducts
+    };
+    res.render('index', data);
 });
 
 app.get('/cursos',(req, res) => {
@@ -17,12 +29,12 @@ app.get('/contacto',(req, res) => {
     res.send('<h1>Aqui puede ver nuestros datos de contacto...</h1>');
 })
 
-app.get('*',(req, res) => { //Duda: como responder con json <<------------------------------------
+app.get('*',(req, res) => {
     //res.status(404).send('No se encuentra la ruta o recurso solicitado');
     res.json({error:'404', message:'No se encuentra la ruta o recurso solicitado'});
 })
 
 //Inicia el servidor
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('Servidor iniciando en el puerto 3000...');
 })
